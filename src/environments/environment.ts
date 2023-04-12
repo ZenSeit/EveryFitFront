a7024e98e46806ac5edb531b8cf12b04ee650ff3
zensei <diegofer_1110@hotmail.com>
2023-04-12 00:16:51 -0500

    settings for firebase and host to connection with backend

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'loginclothes-204ae',
+    appId: '1:273772390720:web:15a7df6762cb2e2f6972a5',
+    storageBucket: 'loginclothes-204ae.appspot.com',
+    apiKey: 'AIzaSyDMDGwsliZ1M66J7n0BwXHMVnDDXhPvVks',
+    authDomain: 'loginclothes-204ae.firebaseapp.com',
+    messagingSenderId: '273772390720',
+  },
+};
+
---
+++ src/environments/settingsToStart.ts
@@ -0,0 +1,3 @@
+export const hostLocal = {
+    host: 'http://localhost:8080/api'
+}
