10c1cc3ddaf5557da02f2ebb5d20267231f56d94
zensei <diegofer_1110@hotmail.com>
2023-04-13 19:21:13 -0500

    catalog done

--- src/app/components/products-grid/products-grid.component.ts
+++ src/app/components/products-grid/products-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClothingItem } from 'src/app/models/clothingItem.model';
 import { ClothingItemsService } from 'src/app/services/clothingItems/clothing-items.service';
+import { ShoppingCartService } from 'src/app/services/shoppingCart/shopping-cart.service';
 
 @Component({
   selector: 'app-products-grid',
@@ -11,32 +12,21 @@ export class ProductsGridComponent implements OnInit {
 
   products:ClothingItem[] = []
 
-  productsInCart:ClothingItem[] = []
-
-  constructor(private clothingService:ClothingItemsService) { }
+  constructor(private clothingService:ClothingItemsService,
+    private shoppingCartService:ShoppingCartService) { }
 
   ngOnInit(): void {
     this.clothingService.getAllClothingItems().subscribe((data:any)=>{
       this.products = data
     })
 
-    this.loadCart()
   }
 
   addProductToCart(product:ClothingItem){
-    console.log(product);
+    this.shoppingCartService.saveCart(product)
   }
 
-  loadCart(){
-    if(localStorage.getItem('cart')){
-      this.productsInCart = JSON.parse(localStorage.getItem('cart') ?? '')
-    }
-  }
-
-  saveCart(myProduct:ClothingItem){
-    this.productsInCart.push(myProduct)
-    localStorage.setItem('cart', JSON.stringify(this.productsInCart))
-  }
+  
 
 
 }
