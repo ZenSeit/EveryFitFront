9722520f6127c5836fe3a8b261d2d1b40687bf95
zensei <diegofer_1110@hotmail.com>
2023-04-14 10:28:21 -0500

    header for web done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,37 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { ClothingItem } from 'src/app/models/clothingItem.model';
+import { Customer } from 'src/app/models/customer.model';
 import { Order } from 'src/app/models/order.model';
 import { ClothingItemsService } from 'src/app/services/clothingItems/clothing-items.service';
+import { CustomerService } from 'src/app/services/customerService/customer.service';
 import { OrderService } from 'src/app/services/order/order.service';
+import { TokenService } from 'src/app/services/tokenService/token.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  customer: Customer | undefined = undefined;
+
+
 
   constructor(private orderService: OrderService,
     private clothingsService: ClothingItemsService,
-    private router: Router
+    private tokenService: TokenService,
+    private customerService: CustomerService,
+    private router: Router,
+    private jwtAuth:JwtHelperService
     ) {}
+
+
+  ngOnInit(): void {
+    this.updateCustomerHeader();
+  }
 
   generateOrder(order: Order) {
     this.orderService.generateOrder(order).subscribe((order) => {
@@ -27,6 +43,25 @@ export class HeaderComponent {
         );
       });
       this.router.navigate(['/ordergenerated'], { queryParams: { data: JSON.stringify(order) } });
+    });
+  }
+
+  updateCustomerHeader(){
+    this.tokenService.loadToken();
+    this.tokenService.getToken().subscribe((token) => {
+      if (token) {
+        this.customerService.getCustomerByEmail(this.jwtAuth.decodeToken(token).email).subscribe((customer) => {
+          this.customer = customer;
+        } );
+      }
+    } );
+  }
+
+  logout() {
+    this.tokenService.closeSession();
+    this.updateCustomerHeader();
+    this.router.navigate([this.router.url], { skipLocationChange: true }).then(() => {
+      window.location.reload();
     });
   }
 
